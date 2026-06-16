78f056737a311bec09ae4b8feb72103ff35609dc
appvalsl <appvalsl@gmail.com>
2026-06-16 16:28:28 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: 'https://zlxznbhoohpfyymiveqf.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpseHpuYmhvb2hwZnl5bWl2ZXFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1OTc0MTgsImV4cCI6MjA5NzE3MzQxOH0.3uHPd5KkSiynaq-GP7P4fIePW-3v7ueck-XcXCKuH-8'
-};
